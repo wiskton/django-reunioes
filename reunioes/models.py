5ac05de82c7b52266139c6bee175c097c6b402b9
Willem Allan <willemarf@gmail.com>
2013-04-10 23:47:54 -0300

    emails

--- reunioes/models.py
+++ reunioes/models.py
@@ -15,3 +15,32 @@ class Reuniao(models.Model):
 
     def __unicode__(self):
         return self.assunto
+
+    def save(self, *args, **kwargs):
+
+        # valores
+        site = 'AVISO REUNIÃO'
+        destino = []
+
+        assunto = self.assunto
+        texto = self.texto
+
+        emails = self.emails.all()
+        print emails
+        for e in emails:
+            destino.append(e.email)
+            print e.nome
+        
+        context = {
+            'site': site,
+            'assunto': assunto,
+            'texto': texto,
+            'data': datetime.datetime.today,
+        }
+
+        email_message = render_to_string('contato.txt', context)
+        from_email = "{0} <{1}>".format(site, settings.EMAIL_HOST_USER)
+
+        # try:
+        send_mail(u'{0} - {1}'.format(site, assunto), email_message, from_email, destino, fail_silently=False)
+        super(Reuniao, self).save(*args, **kwargs)
